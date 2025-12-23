c414b5cda59ad406abd1edde867dbdec4f2080e9
Jade Grant <kadenith@gmail.com>
2025-12-22 19:57:18 -0700

    atempting to fix the oauth redirect

--- supabase/functions/login-bootstrap/index.ts
+++ supabase/functions/login-bootstrap/index.ts
@@ -3,8 +3,11 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, content-type",
+  "Access-Control-Allow-Headers":
+    "authorization, content-type, x-client-info",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
+
 
 serve(async (req) => {
   // 1️⃣ Handle CORS preflight
