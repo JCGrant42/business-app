44ffc2c9c2514db8aac90740a92ce43fd406c90e
Jade Grant <kadenith@gmail.com>
2025-12-27 22:26:06 -0700

    testing create_dashboard func

--- supabase/functions/create_dashboard/index.ts
+++ supabase/functions/create_dashboard/index.ts
@@ -1,5 +1,5 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import { supabase, corsHeaders, handlePreflight, enforcePost, jsonResponse } from "./_helper.ts";
+import { supabase, corsHeaders, handlePreflight, enforcePost, jsonResponse } from "../_helper.ts";
 
 Deno.serve(async (req) => {
   try {
